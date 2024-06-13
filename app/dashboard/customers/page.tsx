486a54d80f6df23556636a9a732bb686f203aba2
ZacSchepis <schezack@isu.edu>
2024-06-12 20:49:16 -0600

    Updated Dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-    return <p>Custoemrs Page</p>
+import { lusitana } from "@/app/ui/fonts";
+import  CustomersTable  from "@/app/ui/customers/table";
+import { Suspense } from "react";
+
+export default function Page({
+    searchParams, 
+} : {
+    searchParams? : {
+        query?: string;
+        page?: string;
+    }
+}) {
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page) || 1;
+    // const totalPages = await fetchCustomersPages(query);
+    return (
+        <div className="w-full">
+            <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                {/* <Search placeholder="Search customers..." /> */}
+            </div>
+            <div>
+                <Suspense key={query + currentPage} fallback={<div>Loading...</div>}>
+                    <CustomersTable query={query} currentPage={currentPage} />
+                </Suspense>
+            </div>
+        </div>
+    )
 }
